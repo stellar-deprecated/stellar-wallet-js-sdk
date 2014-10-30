822f6de50e7c4434427e09515ae14891aab8b393
Bartek Nowotarski <bartek@nowotarski.info>
2014-10-30 14:23:51 +0100

    Generate keyPair from seed

--- test/util/keypair.js
+++ test/util/keypair.js
@@ -14,6 +14,21 @@ describe('util/keypair', function () {
     expect(nacl.verify(keyPair.publicKey, keyPairFromSecret.publicKey)).to.be.true;
     expect(keyPair.address).not.to.be.empty;
     expect(keyPair.address.charAt(0)).to.be.equal('g');
+    expect(keyPair.secret.charAt(0)).to.be.equal('s');
+    done();
+  });
+
+  it('should generate a keypair from seed', function (done) {
+    var seed = 's3sYSKfA2xuEWFxdv81YcemGdVgFyZWekRTibbo3pD7BVAisF6Q';
+    var keyPair = keypair.generateKeyPair(seed);
+    expect(keyPair.secret).to.be.equal(seed);
+    keyPair.publicKey = nacl.util.decodeBase64(keyPair.publicKey);
+    keyPair.secretKey = nacl.util.decodeBase64(keyPair.secretKey);
+
+    var keyPairFromSecret = nacl.sign.keyPair.fromSecretKey(keyPair.secretKey);
+    expect(nacl.verify(keyPair.publicKey, keyPairFromSecret.publicKey)).to.be.true;
+    expect(keyPair.address).not.to.be.empty;
+    expect(keyPair.address.charAt(0)).to.be.equal('g');
     done();
   });
 });
